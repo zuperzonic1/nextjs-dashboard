906635211459c882f117c4a1b993f3a8e9990275
Micheal Felobes <65277151+zuperzonic1@users.noreply.github.com>
2024-02-13 12:21:41 -0500

    Add fetchLatestInvoices function and uncomment LatestInvoices component

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -36,7 +36,7 @@ export async function fetchLatestInvoices() {
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email
-      FROM invoice
+      FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
