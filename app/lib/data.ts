169cb4d13fd445aa61f850fb44fe22469cb37b58
Micheal Felobes <65277151+zuperzonic1@users.noreply.github.com>
2024-02-13 12:05:55 -0500

    Update dependencies and fix revenue chart

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -35,7 +35,7 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   try {
     const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+      SELECT invoices.amount, customers.name, customers.image_url, customers.email
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
